ae48f293b384f7d57496fe6f99fd6cfdc8b38939
Muhammad Norzariman Razari <tr32010@gmail.com>
2026-04-16 14:26:39 +0800

    Remove comment

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Terminal SDK",
+    name: "TerminalSDK",
     platforms: [
-        .iOS("15.0")
+        .iOS(.v15)
     ],
     products: [
         // We export the library under the name "TerminalC2C".
@@ -12,7 +12,7 @@ let package = Package(
         .library(
             name: "TerminalC2C",
             targets: ["TerminalC2CWrapper"]
-        )
+        ),
         .library(
             name: "TerminalH2H",
             targets: ["TerminalH2HWrapper"]
@@ -22,37 +22,37 @@ let package = Package(
         .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.57.3")
     ],
     targets: [
-        // 1. The actual binary framework.
-        // We give it a unique name (TerminalC2CBinary) to avoid
-        // namespace collisions with the wrapper target or the product name.
+        // 1. The actual binary frameworks.
+        // We give them unique names to avoid namespace collisions with the wrapper targets.
         .binaryTarget(
             name: "TerminalC2CBinary",
-            url: "https://github.com/xendit/xendit-terminal-ios/releases/download/1.0.0/TerminalC2C.xcframework.zip",
+            url: "https://github.com/shiburagi/cocoapod-test-2/releases/download/1.0.0/TerminalC2C.xcframework.zip",
             checksum: "a7836930dce347e1641c4daadbcf927d00919b3e219f695b0fcba7fe72120b98"
         ),
         .binaryTarget(
             name: "TerminalH2HBinary",
-            url: "https://github.com/xendit/xendit-terminal-ios/releases/download/1.0.0/TerminalH2H.xcframework.zip",
+            url: "https://github.com/shiburagi/cocoapod-test-2/releases/download/1.0.0/TerminalH2H.xcframework.zip",
             checksum: "f6f90bee90db6ad1b0c691027e9211a568de71ac0f39b22ab8ba63b324136dfc"
         ),
 
-        // 2. The wrapper target.
-        // This is the target that the consuming app actually "sees" and builds.
+        // 2. The wrapper targets.
+        // These are the targets that the consuming app actually "sees" and builds.
+        // They must have unique paths.
         .target(
             name: "TerminalC2CWrapper",
             dependencies: [
                 .target(name: "TerminalC2CBinary"),
                 .product(name: "Sentry", package: "sentry-cocoa")
             ],
-            path: "Sources/TerminalWrapper"
-        )
+            path: "Sources/TerminalC2CWrapper"
+        ),
         .target(
             name: "TerminalH2HWrapper",
             dependencies: [
                 .target(name: "TerminalH2HBinary"),
                 .product(name: "Sentry", package: "sentry-cocoa")
             ],
-            path: "Sources/TerminalWrapper"
+            path: "Sources/TerminalH2HWrapper"
         )
     ]
 )
--- Sources/TerminalWrapper/Export.swift
+++
@@ -1,5 +0,0 @@
-//
-// Created by Muhammad Norzariman Razari on 10/02/2026.
-//
-
-import Foundation
